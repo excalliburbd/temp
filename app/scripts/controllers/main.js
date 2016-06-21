50c26f3a5d9cfa02b65b451ff59a9616c51c0eb5
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2016-06-21 11:01:41 +0000

    ;e;

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name technoanimalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the technoanimalApp
+ */
+angular.module('technoanimalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
